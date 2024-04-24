f7abe6675d1157a8446ac21f2cfb8435221b1eb4
Puru <awesomepuruvj@gmail.com>
2024-04-24 23:47:35 +0530

    Pointer events polyfill, touch action manipulation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
 	import { Toaster } from 'svelte-french-toast';
 	import '../lib/global.css';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (!('PointerEvent' in window)) {
+			// @ts-ignore
+			await import('@wessberg/pointer-events');
+		}
+	});
 </script>
 
 <slot />
